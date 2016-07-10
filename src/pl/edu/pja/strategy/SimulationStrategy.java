b670e511219909e64dc686986c1475f2dd3baadc
Paweł Dyda <pawel.dyda@gazeta.pl>
2016-07-10 08:36:31 +0200

    Simulation strategy.

---
+++ src/pl/edu/pja/strategy/SimulationStrategy.java
@@ -0,0 +1,11 @@
+package pl.edu.pja.strategy;
+
+public enum SimulationStrategy {
+
+    NO_GENDER_QUOTA,
+    NO_WOMEN_MANAGERS,
+    FIFTY_PERCENT_WOMEN_MANAGERS,
+    SEVENTY_FIVE_PERCENT_WOMEN_MANAGERS,
+    ONE_HUNDRED_PERCENT_WOMEN_MANAGERS
+
+}
